af58f3678ee0a683758ba468f9a7a0133ce5b7c4
jbshin <josephshin93@gmail.com>
2016-10-09 23:58:38 -0400

    updated a location marker with a custom icon

--- CDV-html5-boilerplate/js/main.js
+++ CDV-html5-boilerplate/js/main.js
@@ -6,6 +6,15 @@ var centerOfBostonCoordinates = [42.3601, -71.0589],
     pjfanLocationCoordinates = [42.3629, -71.0890],
     mapboxAPIToken = prompt("Please enter jbshin's mapbox API token", "API Token");
 
+var pjfanMarkerIcon = L.icon({
+    iconUrl: '../pjfan_marker_icon.png',
+    shadowURL: '../pjfan_marker_icon_shadow.png',
+    iconSize: [60, 80],
+    shadowSize: [40, 40],
+    iconAnchor: [30, 79],
+    shadowAnchor: [1, 1]
+});
+
 var mymap = L.map('mapid').setView(centerOfBostonCoordinates, 11);
 
 L.mapbox.accessToken = mapboxAPIToken;
@@ -13,7 +22,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     accessToken: mapboxAPIToken
 }).addTo(mymap);
-var pjfanLocationMarker = L.marker(pjfanLocationCoordinates).addTo(mymap);
+var pjfanLocationMarker = L.marker(pjfanLocationCoordinates, {icon: pjfanMarkerIcon}).addTo(mymap);
 pjfanLocationMarker.bindPopup("<b>Peter \"BBOY\" Fan's Location</b>");
 
 
